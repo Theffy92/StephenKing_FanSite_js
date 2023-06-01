cff0f7bd416537d52a848ae26a2b726bfc4ff0b9
Elena Estefania Reyes <estefyreyes.92@gmail.com>
2023-05-31 23:57:29 -0400

    add code to index, js and css, create gitignore

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// Changing the text content
+
+const appearanceHeading = document.getElementById("fun__heading");
+
+appearanceHeading.textContent = "Fun Facts";
+
+// Slide show behavior
+
+let slideIndex = 0;
+
+
+
+const showSlides = () =>{
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for(let i= 0; i < slides.length; i++){
+        slides[i].style.display = "none"
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+showSlides();
